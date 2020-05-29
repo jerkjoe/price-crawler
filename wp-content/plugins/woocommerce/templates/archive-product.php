dd2cb72b9faed1c2c636db524fa45660e6c9793d
Joseph Jin <jerkjoe61@gmail.com>
2020-05-28 22:11:35 -0700

    category

--- wp-content/plugins/woocommerce/templates/archive-product.php
+++ wp-content/plugins/woocommerce/templates/archive-product.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The Template for displaying product archives, including the main shop page which is a post type archive
  *
@@ -15,9 +16,9 @@
  * @version 3.4.0
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-get_header( 'shop' );
+get_header('shop');
 
 /**
  * Hook: woocommerce_before_main_content.
@@ -26,72 +27,183 @@ get_header( 'shop' );
  * @hooked woocommerce_breadcrumb - 20
  * @hooked WC_Structured_Data::generate_website_data() - 30
  */
-do_action( 'woocommerce_before_main_content' );
+do_action('woocommerce_before_main_content');
 
 ?>
-<?php 
-    $queries = array();
-    parse_str($_SERVER['QUERY_STRING'], $queries);
-    var_dump($queries);
-    $shop_page_url = get_permalink( wc_get_page_id( 'shop' ) );
-    // Get Woocommerce product categories WP_Term objects
-    $categories = get_terms(['taxonomy' => 'product_cat']);
-    // print_r($categories);
+<?php
+$queries = array();
+parse_str($_SERVER['QUERY_STRING'], $queries);
+// var_dump($queries);
+$shop_page_url = get_permalink(wc_get_page_id('shop'));
+// Get Woocommerce product categories WP_Term objects
+$categories = get_terms(['taxonomy' => 'product_cat']);
+// print_r($categories);
 
-    $args = array(
+$args = array(
+    'taxonomy' => 'product_cat',
+    'hide_empty' => false,
+    'parent'   => 0
+);
+$product_cat = get_terms($args);
+
+foreach ($product_cat as $parent_product_cat) {
+
+    // echo '
+    //     <ul>
+    //     <li><a href="'.$shop_page_url.'?category='.$parent_product_cat->name.'">'.$parent_product_cat->name.'</a>
+    //     <ul>
+    //         ';
+    $child_args = array(
         'taxonomy' => 'product_cat',
         'hide_empty' => false,
-        'parent'   => 0
+        'parent'   => $parent_product_cat->term_id
     );
-    $product_cat = get_terms( $args );
+    $child_product_cats = get_terms($child_args);
+    $parent_product_cat->children = $child_product_cats;
+    // foreach ($child_product_cats as $child_product_cat)
+    // {
+    // echo '<li><a href="'.$shop_page_url.'?category='.$child_product_cat->name.'">'.$child_product_cat->name.'</a></li>';
+    // }
+    // print_r($product_cat);
+    // echo(json_encode($product_cat));
+    // echo '</ul>
+    //     </li>
+    // </ul>';
+}
 
-    foreach ($product_cat as $parent_product_cat)
-    {
 
-    echo '
-        <ul>
-        <li><a href="'.$shop_page_url.'?category='.$parent_product_cat->name.'">'.$parent_product_cat->name.'</a>
-        <ul>
-            ';
-    $child_args = array(
-                'taxonomy' => 'product_cat',
-                'hide_empty' => false,
-                'parent'   => $parent_product_cat->term_id
-            );
-    $child_product_cats = get_terms( $child_args );
-    foreach ($child_product_cats as $child_product_cat)
-    {
-    echo '<li><a href="'.$shop_page_url.'?category='.$child_product_cat->name.'">'.$child_product_cat->name.'</a></li>';
-    }
+// var_dump($queries);
+// echo $queries['category'];
+// echo '[product_category category="" per_page="8"' . $queries['category'] . 'columns="2" orderby="date" order="desc"]';
 
-    echo '</ul>
-        </li>
-    </ul>';
-    }
-    
-    
-    var_dump($queries);
-    echo $queries['category'];
-    echo '[product_category category="" per_page="8"' . $queries['category'] . 'columns="3" orderby="date" order="desc"]';
-    echo do_shortcode('[product_category category="'.$queries['category'].'" per_page="8" columns="3" orderby="date" order="desc"]'); 
+
 ?>
 <header class="woocommerce-products-header">
-	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
-	<?php endif; ?>
+    <?php if (apply_filters('woocommerce_show_page_title', true)) : ?>
+        <!-- <h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1> -->
+    <?php endif; ?>
 
-	<?php
-	/**
-	 * Hook: woocommerce_archive_description.
-	 *
-	 * @hooked woocommerce_taxonomy_archive_description - 10
-	 * @hooked woocommerce_product_archive_description - 10
-	 */
-	do_action( 'woocommerce_archive_description' );
-	?>
+    <?php
+    /**
+     * Hook: woocommerce_archive_description.
+     *
+     * @hooked woocommerce_taxonomy_archive_description - 10
+     * @hooked woocommerce_product_archive_description - 10
+     */
+    // do_action('woocommerce_archive_description');
+    ?>
 </header>
-<?php
+<style>
+    .woocommerce-breadcrumb {
+        display: none;
+    }
+</style>
 
+<body>
+    <div id="categories"">
+        <div id="left"></div>
+        <div id="right"></div>
+    </div>
+</body>
+<script>
+    console.log(123)
+</script>
+<?php
+if ($queries['category'] != '') {
+    echo do_shortcode('[product_category category="' . $queries['category'] . '" per_page="2" columns="3" orderby="date" order="desc" paginate=true]');
+} else {
+?>
+    <style>
+        #categories {
+            position: fixed;
+            left: 0;
+            top: 0;
+            z-index: 99999;
+            background-color: #ffff;
+            width: 100%;
+            height: 100vh;
+            display: flex;
+        }
+
+        #left {
+            overflow: auto;
+            border-right: 1px solid #e3e3e3;
+            background-color: #f0f2f5;
+        }
+
+        #right {
+            flex: 1;
+        }
+
+        .right-container {
+            height: 100%;
+            width: 100%;
+            display: none;
+            flex-direction: column;
+            overflow: auto;
+        }
+
+        .right-container>a {
+            padding: 10px;
+            border-bottom: 1px solid #e3e3e3;
+        }
+
+        .category-item {
+            padding: 10px 14px;
+            border-bottom: 1px solid #e9e9e9;
+        }
+    </style>
+
+
+    <script>
+        var categories = <?php echo json_encode($product_cat) ?>;
+        document.addEventListener('DOMContentLoaded', function() {
+            console.log(123123123)
+            console.log(categories)
+            console.log(container)
+            var container = document.getElementById('left')
+            var detailContainer = document.getElementById('right')
+            for (var i = 0; i < categories.length; i++) {
+                container.appendChild(generateParentElement(categories[i]))
+                detailContainer.appendChild(createContainer(categories[i]))
+                if (categories[i].children.length) {
+                    console.log(categories[i].children)
+                    for (var j = 0; j < categories[i].children.length; j++) {
+                        var el = document.createElement('a')
+                        el.href = window.location.origin + window.location.pathname + '?category=' + categories[i].children[j].name
+                        el.innerText = categories[i].children[j].name
+                        document.querySelector('.c_' + categories[i].children[j].parent).appendChild(el)
+                    }
+                }
+            }
+        })
+
+        function generateParentElement(item) {
+            var el = document.createElement('div')
+            el.className = 'category-item'
+            el.id = item.term_id
+            el.innerText = item.name
+            el.addEventListener('click', function(e) {
+                var selector = '.c_' + e.target.id
+                console.log('123 ', selector)
+                Array.from(document.querySelectorAll('.right-container')).forEach(e => {
+                    e.style.display = 'none'
+                })
+                document.querySelector(selector).style.display = 'flex'
+            })
+            return el
+        }
+
+        function createContainer(item) {
+            var el = document.createElement('div')
+            el.className = 'c_' + item.term_id + ' right-container'
+            var url = window.location.origin + window.location.pathname + '?category=' + item.name
+            el.innerHTML = '<a href="' + url + '">All</a>'
+            return el
+        }
+    </script>
+<?php
+}
 
 
 
@@ -145,13 +257,13 @@ do_action( 'woocommerce_before_main_content' );
  *
  * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
  */
-do_action( 'woocommerce_after_main_content' );
+do_action('woocommerce_after_main_content');
 
 /**
  * Hook: woocommerce_sidebar.
  *
  * @hooked woocommerce_get_sidebar - 10
  */
-do_action( 'woocommerce_sidebar' );
+do_action('woocommerce_sidebar');
 
-get_footer( 'shop' );
+get_footer('shop');
